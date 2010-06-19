96f6dcac258f61703afe4aff5eb06a68af55e7cf
Andrea Piccinelli <frasten@gmail.com>
2010-06-19 16:25:30 +0200

    Some steps forward. Now drag&drop works.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- extra-nav-icons.php
+++ extra-nav-icons.php
@@ -106,8 +106,22 @@ class Mystique_Extra_Nav_Icons {
 			        'administrator', $this->plugin_slug,
 			        array( &$this, 'options_page' )
 			);
+
+			/* Using registered $page handle to hook script load */
+			add_action( 'admin_print_scripts-' . $page, array( &$this, 'admin_options_scripts' ) );
 		}
 	}
+
+
+	/**
+	 * Loads needed scripts for admin interface.
+	 */
+	function admin_options_scripts() {
+		/* ui.sortable.js from jQuery UI v1.7.1 provided with WP <= 2.9.2 is
+		 * buggy. So I'm using a custom version, backporting a fix to bug #4551 */
+		wp_enqueue_script( 'menijQueryUIsortable', $this->plugin_url . '/js/ui.sortable.js', array( 'jquery-ui-core' ) );
+	}
+
 
 	/**
 	 * Adds a link to the configuration page in the plugins list.
@@ -127,9 +141,74 @@ class Mystique_Extra_Nav_Icons {
 	}
 
 	function options_page() {
+		printf( "<div class='wrap'>\n<h2>%s</h2>", __( 'Mystique Extra Nav Icons settings', $this->plugin_slug ) );
+
+
+		echo "<ul id='stt_enabled_smilies' class='sttSortable'>\n";
+		// Get the visible smilies in the (eventually) custom order
 		foreach ( $this->default_icons as $icon ) {
-			echo "<img src='{$this->plugin_url}/icons/nav-$icon.png' />\n";
+			echo "<li id='sttelement|$image'><img src='{$this->plugin_url}/icons/nav-$icon.png'";
+			echo " class='wp-smiley'";
+			echo " alt='" . str_replace( "'", '&#039;', $text ) . "'";
+			echo " title='" . str_replace( "'", '&#039;', $text ) . "'";
+			echo " /></li>\n";
 		}
+		#$this->__print_ordered_smilies( $sm[0] );
+		echo "</ul>\n";
+
+		echo "<br style='clear:both'/>\n";
+
+		echo '<h3>' . __( "Hidden smilies:", $this->plugin_slug ) . '</h3>';
+		echo "<ul id='stt_disabled_smilies' class='sttSortable'>\n";
+		// Get the hidden smilies in the (eventually) custom order
+		#$this->__print_ordered_smilies( $sm[1] );
+		echo "</ul>";
+
+
+		?>
+<style type="text/css">
+	.sttSortable { list-style-type: none; margin: 10px 10px 20px; padding: 5px; width: 50%;min-height: 20px;}
+	.sttSortable .wp-smiley {cursor: move;}
+	#stt_enabled_smilies {background-color: #cfc;border: 1px solid #8a8;float: left;}
+	#stt_disabled_smilies {background-color: #aaa;border: 1px solid #666;float: left;}
+	#stt_enabled_smilies li, #stt_disabled_smilies li { margin: 3px 3px 3px 0; padding: 1px; display: block; float: left; border: 1px solid #ddd}
+</style>
+<script type="text/javascript">
+/* <![CDATA[ */
+(function($) {
+	$("#stt_enabled_smilies, #stt_disabled_smilies").sortable({
+		connectWith: '.sttSortable',
+		stop: function (event, ui) {
+			enabled_order = $("#stt_enabled_smilies").sortable('toArray');
+			disabled_order = $("#stt_disabled_smilies").sortable('toArray');
+			for (i = 0;i < enabled_order.length;i++) {
+				enabled_order[i] = enabled_order[i].split('|')[1]
+			}
+			for (i = 0;i < disabled_order.length;i++) {
+				disabled_order[i] = disabled_order[i].split('|')[1]
+			}
+			$("#stt_ajax_status").css('visibility', '');
+			$.ajax({
+				type: 'post',
+				url: 'admin-ajax.php',
+				traditional: true,
+				data: {
+					'sttenabled[]': enabled_order,
+					'sttdisabled[]': disabled_order,
+					action: 'stt_saveorder',
+					_ajax_nonce: '<?php echo wp_create_nonce( 'stt_order' ) ?>'
+				},
+				success: function(data) {
+					$("#stt_ajax_status").css('visibility', 'hidden');
+				}
+			});
+		}
+	}).disableSelection();
+})(jQuery);
+/* ]]> */
+</script>
+<?php
+		echo '</div>';
 	}
 
 	/**
