539e11bf0a206839cb7564743e23a4ec05438f35
Andrea Piccinelli <frasten@gmail.com>
2010-06-22 16:29:14 +0200

    New internal structure.
    Signed-off-by: Andrea Piccinelli <frasten@gmail.com>

--- extra-nav-icons.php
+++ extra-nav-icons.php
@@ -10,70 +10,96 @@ Author URI: http://polpoinodroidi.com
 License: GPL3
 */
 
+/* Internal data structure */
+if ( ! class_exists( 'MENI_Icon' ) ):
+class MENI_Icon {
+	var $name;
+	var $url;
+	var $text;
+	function MENI_Icon( $name, $url, $text ) {
+		// Constructor
+		$this->name = $name;
+		$this->url = $url;
+		$this->text = $text;
+	}
+}
+endif;
+
+
 if ( ! class_exists( 'Mystique_Extra_Nav_Icons' ) ):
 class Mystique_Extra_Nav_Icons {
 	var $plugin_slug = 'mystique-extra-nav-icons';
 	var $plugin_url;
-	var $default_icons = array(
-		'android' => '',
-		'apple' => '',
-		'beer' => '',
-		'blogger' => 'http://YOUR_USERNAME.blogspot.com/',
-		'coffee' => '',
-		'contact' => '',
-		'dailymotion' => 'http://dailymotion.com/YOUR_USERNAME',
-		'delicious' => 'http://delicious.com/YOUR_USERNAME',
-		'digg' => 'http://digg.com/users/YOUR_USERNAME',
-		'discogs' => 'http://www.discogs.com/artist/YOUR_USERNAME',
-		'email' => 'mailto:YOUR_EM@IL.COM',
-		'email_subscription' => '',
-		'facebook' => 'http://www.facebook.com/YOUR_USERNAME',
-		'flickr' => 'http://www.flickr.com/photos/YOUR_USERNAME',
-		'formspring' => 'http://www.formspring.me/YOUR_USERNAME',
-		'friendfeed' => 'http://friendfeed.com/YOUR_USERNAME',
-		'github' => 'http://github.com/YOUR_USERNAME',
-		'gmail' => 'mailto:YOUR_USERNAME@gmail.com',
-		'goodreads' => 'http://www.goodreads.com/user/show/YOUR_USERID',
-		'googlebuzz' => 'http://www.google.com/profiles/YOUR_USERNAME',
-		'googlewave' => '',
-		'hyves' => 'http://YOUR_USERNAME.hyves.nl/',
-		'identica' => 'http://identi.ca/YOUR_USERNAME',
-		'lastfm' => 'http://www.lastfm.it/user/YOUR_USERNAME',
-		'linkedin' => 'http://linkedin.com/in/YOUR_PUBLIC_PROFILE',
-		'mirc' => '',
-		'myspace' => 'http://www.myspace.com/YOUR_USERNAME',
-		'paypal' => '',
-		'picasa' => 'http://picasaweb.google.it/YOUR_USERNAME',
-		'rss' => '',
-		'skype' => 'skype:YOUR_USERNAME',
-		'slideshare' => 'http://www.slideshare.net/YOUR_USERNAME',
-		'soundcloud' => 'http://soundcloud.com/YOUR_USERNAME',
-		'sourceforge' => 'http://YOUR_PROJECT.sourceforge.net/',
-		'spotify' => 'spotify:artist:YOUR_ID',
-		'steam' => '',
-		'twitter' => 'http://twitter.com/YOUR_USERNAME',
-		'ubuntu' => 'http://ubuntuforums.org/member.php?u=YOUR_USERID',
-		'vimeo' => 'http://vimeo.com/YOUR_USERNAME',
-		'wiki' => 'http://en.wikipedia.org/wiki/CHANGE_ME',
-		'windows_live' => 'http://YOUR_USERNAME.profile.live.com/',
-		'xing' => 'http://www.xing.com/profile/YOUR_USERNAME',
-		'yahoo' => 'http://pulse.yahoo.com/YOUR_USERID',
-		'youtube' => 'http://www.youtube.com/user/YOUR_USERNAME',
-	);
+	var $default_icons;
 
 	function init() {
 		$dir = basename(dirname(__FILE__));
 		$this->plugin_url = WP_PLUGIN_URL . "/$dir";
-		$this->default_icons['rss'] = get_bloginfo( 'rss2_url' );
+
+		$this->default_icons = array(
+			new MENI_Icon( 'android', '', '' ),
+			new MENI_Icon( 'apple', '', '' ),
+			new MENI_Icon( 'beer', '', '' ),
+			new MENI_Icon( 'blogger', 'http://YOUR_USERNAME.blogspot.com/', '' ),
+			new MENI_Icon( 'coffee', '', '' ),
+			new MENI_Icon( 'contact', '', '' ),
+			new MENI_Icon( 'dailymotion', 'http://dailymotion.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'delicious', 'http://delicious.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'digg', 'http://digg.com/users/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'discogs', 'http://www.discogs.com/artist/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'email', 'mailto:YOUR_EM@IL.COM', '' ),
+			new MENI_Icon( 'email_subscription', '', '' ),
+			new MENI_Icon( 'facebook', 'http://www.facebook.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'flickr', 'http://www.flickr.com/photos/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'formspring', 'http://www.formspring.me/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'friendfeed', 'http://friendfeed.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'github', 'http://github.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'gmail', 'mailto:YOUR_USERNAME@gmail.com', '' ),
+			new MENI_Icon( 'goodreads', 'http://www.goodreads.com/user/show/YOUR_USERID', '' ),
+			new MENI_Icon( 'googlebuzz', 'http://www.google.com/profiles/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'googlewave', '', '' ),
+			new MENI_Icon( 'hyves', 'http://YOUR_USERNAME.hyves.nl/', '' ),
+			new MENI_Icon( 'identica', 'http://identi.ca/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'lastfm', 'http://www.lastfm.it/user/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'linkedin', 'http://linkedin.com/in/YOUR_PUBLIC_PROFILE', '' ),
+			new MENI_Icon( 'mirc', '', '' ),
+			new MENI_Icon( 'myspace', 'http://www.myspace.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'paypal', '', '' ),
+			new MENI_Icon( 'picasa', 'http://picasaweb.google.it/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'rss', get_bloginfo( 'rss2_url' ), '' ),
+			new MENI_Icon( 'skype', 'skype:YOUR_USERNAME', '' ),
+			new MENI_Icon( 'slideshare', 'http://www.slideshare.net/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'soundcloud', 'http://soundcloud.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'sourceforge', 'http://YOUR_PROJECT.sourceforge.net/', '' ),
+			new MENI_Icon( 'spotify', 'spotify:artist:YOUR_ID', '' ),
+			new MENI_Icon( 'steam', '', '' ),
+			new MENI_Icon( 'twitter', 'http://twitter.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'ubuntu', 'http://ubuntuforums.org/member.php?u=YOUR_USERID', '' ),
+			new MENI_Icon( 'vimeo', 'http://vimeo.com/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'wiki', 'http://en.wikipedia.org/wiki/CHANGE_ME', '' ),
+			new MENI_Icon( 'windows_live', 'http://YOUR_USERNAME.profile.live.com/', '' ),
+			new MENI_Icon( 'xing', 'http://www.xing.com/profile/YOUR_USERNAME', '' ),
+			new MENI_Icon( 'yahoo', 'http://pulse.yahoo.com/YOUR_USERID', '' ),
+			new MENI_Icon( 'youtube', 'http://www.youtube.com/user/YOUR_USERNAME', '' ),
+		);
+
 		// FOR DEBUG PURPOSES:
 		//delete_option($this->plugin_slug);
 	}
 
 
 	function get_nav( $nav_extra ) {
+		// TODO: choose some default values to show
+		list( $en_order, $dis_order) = $this->get_ordered_list();
+
 		$nav_extra = '';
-		
-		$nav_extra .= '<a href="'.get_bloginfo('rss2_url').'" class="nav-extra rss" title="RSS Feeds"><span>RSS Feeds</span></a>';
+		foreach ( $en_order as $icon ) {
+			$nav_extra .= "<a href='" . esc_attr( $icon->url ) .
+				"' class='nav-extra nav-{$icon->name}' title='" .
+				esc_attr( $icon->text ) . "'><span>" .
+				esc_html( $icon->text ) . "</span></a>\n";
+		}
+
 		return $nav_extra;
 	}
 
@@ -103,6 +129,7 @@ class Mystique_Extra_Nav_Icons {
 		$settings[$key] = $value;
 		update_option( $this->plugin_slug, $settings );
 	}
+
 
 	/**
 	 * Creates a link to the plugin's configuration page.
@@ -150,6 +177,7 @@ class Mystique_Extra_Nav_Icons {
 		return $links;
 	}
 
+
 	function options_page() {
 		printf( "<div class='wrap'>\n<h2>%s</h2>", __( 'Mystique Extra Nav Icons settings', $this->plugin_slug ) );
 
@@ -157,12 +185,14 @@ class Mystique_Extra_Nav_Icons {
 
 		echo '<h3>' . __( "Enabled icons:", $this->plugin_slug ) . '</h3>';
 		echo "<ul id='meni_enabled_icons' class='iconSortable'>\n";
-		// Get the visible smilies in the (eventually) custom order
-		foreach ( $en_order as $icon => $url ) {
-			echo "<li id='sttelement|$icon'><img src='{$this->plugin_url}/icons/nav-$icon.png'";
-			echo " class='wp-smiley'";
-			echo " alt='" . str_replace( "'", '&#039;', $text ) . "'";
-			echo " title='" . str_replace( "'", '&#039;', $text ) . "'";
+		// Get the visible icons in the (eventually) custom order
+		// TODO: Avoid duplicating this, put it into a function
+		foreach ( $en_order as $icon ) {
+			echo "<li id='menielement|{$icon->name}'>";
+			echo "<img src='{$this->plugin_url}/icons/nav-{$icon->name}.png'";
+			// TODO: also update alt&title on change, if it's worthwhile
+			echo " alt='" . esc_attr( $icon->text ) . "'";
+			echo " title='" . esc_attr( $icon->text ) . "'";
 			echo " /></li>\n";
 		}
 		echo "</ul>\n";
@@ -170,7 +200,8 @@ class Mystique_Extra_Nav_Icons {
 		echo "<div class='meni_icon_settings'>";
 		echo "<div id='meni_url_form'>";
 		echo "<span></span>";
-		echo "<input type='text' id='meni_url_textbox' />";
+		echo "<input type='text' id='meni_url_textbox' /><br/>";
+		echo "<input type='text' id='meni_text_textbox' />";
 		echo "</div>";
 		echo "<div id='meni_saving'>";
 		echo "<img src='{$this->plugin_url}/ajax-loader.gif' alt='Loading' /> ";
@@ -183,17 +214,17 @@ class Mystique_Extra_Nav_Icons {
 
 		echo '<h3>' . __( "Disabled icons:", $this->plugin_slug ) . '</h3>';
 		echo "<ul id='meni_disabled_icons' class='iconSortable'>\n";
-		foreach ( $dis_order as $icon => $url ) {
-			echo "<li id='sttelement|$icon'><img src='{$this->plugin_url}/icons/nav-$icon.png'";
-			echo " class='wp-smiley'";
-			echo " alt='" . str_replace( "'", '&#039;', $text ) . "'";
-			echo " title='" . str_replace( "'", '&#039;', $text ) . "'";
+		foreach ( $dis_order as $icon ) {
+			echo "<li id='menielement|{$icon->name}'>";
+			echo "<img src='{$this->plugin_url}/icons/nav-{$icon->name}.png'";
+			echo " alt='" . esc_attr( $icon->text ) . "'";
+			echo " title='" . esc_attr( $icon->text ) . "'";
 			echo " /></li>\n";
 		}
 		echo "</ul>";
 
 
-		?>
+?>
 <style type="text/css">
 	.iconSortable { list-style-type: none; margin: 10px 10px 20px; padding: 5px; width: 50%;min-height: 20px;}
 	.iconSortable img {cursor: move;}
@@ -204,16 +235,17 @@ class Mystique_Extra_Nav_Icons {
 	#meni_url_form {padding: 5px;border: 1px solid #666;background-color: #ddd;}
 	#meni_saving {display: none;margin-top: 4px;}
 	.meni_icon_settings {float: left; margin-top: 10px;margin-left: 20px;width: 40%;}
-	#meni_url_textbox {width: 100%;}
+	#meni_url_textbox, #meni_text_textbox {width: 100%;}
 </style>
 <script type="text/javascript">
 /* <![CDATA[ */
-var meni_urls = {
+var meni_icons = {
 <?php
 	$temp = array();
-	$urls = $this->merge_urls_from_db();
-	foreach ( $urls as $icon => $url ) {
-		$temp[] = "$icon: '" . esc_attr( $url ) . "'";
+	$icons = $this->merge_icons_from_db();
+	foreach ( $icons as $icon ) {
+		$temp[] = "{$icon->name}: new Array('" . esc_attr( $icon->url ) . "', '" .
+		  esc_attr( $icon->text ) . "')";
 	}
 	echo implode( ",\n", $temp);
 ?>
@@ -255,30 +287,42 @@ var meni_selected_icon;
 		$(this).parent().addClass('iconSortableSelected');
 		var url = $(this).attr('src');
 		meni_selected_icon = /\/nav-(.+)\.png$/.exec(url)[1];
-		$("#meni_url_textbox").val(meni_urls[meni_selected_icon]);
+		$("#meni_url_textbox").val(meni_icons[meni_selected_icon][0]);
+		$("#meni_text_textbox").val(meni_icons[meni_selected_icon][1]);
+		meni_oldvalue[0] = meni_icons[meni_selected_icon][0];
+		meni_oldvalue[1] = meni_icons[meni_selected_icon][1];
 		$(".meni_icon_settings span").html("");
 	});
 
 	var meni_keypress_timeout;
-	var meni_oldvalue;
-	$("#meni_url_textbox").keyup(function (e) {
-		meni_urls[meni_selected_icon] = $("#meni_url_textbox").val();
+	var meni_oldvalue = new Array('', '');
+	$("#meni_url_textbox, #meni_text_textbox").keyup(function (e) {
+		meni_icons[meni_selected_icon][0] = $("#meni_url_textbox").val();
+		meni_icons[meni_selected_icon][1] = $("#meni_text_textbox").val();
 		if (meni_keypress_timeout != undefined) {
 			clearTimeout(meni_keypress_timeout);
 		}
 		meni_keypress_timeout = setTimeout(function() {
-			if (meni_oldvalue == meni_urls[meni_selected_icon]) return;
+			if (meni_oldvalue[0] == meni_icons[meni_selected_icon][0] &&
+			  meni_oldvalue[1] == meni_icons[meni_selected_icon][1]) {
+				return;
+			}
 			meni_keypress_timeout = undefined;
 			$("#meni_saving").show();
-			meni_urls.action = 'meni_saveurls';
-			meni_urls._ajax_nonce = '<?php echo wp_create_nonce( 'meni_urls' ) ?>';
 			$.ajax({
 				type: 'post',
 				url: 'admin-ajax.php',
-				data: meni_urls,
+				data: {
+					action: 'meni_saveicons',
+					_ajax_nonce: '<?php echo wp_create_nonce( 'meni_icons' ) ?>',
+					name: meni_selected_icon,
+					text: meni_icons[meni_selected_icon][1],
+					url: meni_icons[meni_selected_icon][0]
+				},
 				success: function(data) {
 					$("#meni_saving").hide();
-					meni_oldvalue = meni_urls[meni_selected_icon];
+					meni_oldvalue[0] = meni_icons[meni_selected_icon][0];
+					meni_oldvalue[1] = meni_icons[meni_selected_icon][1];
 				}
 			});
 		}, 500);
@@ -293,17 +337,24 @@ var meni_selected_icon;
 	}
 
 
-	function merge_urls_from_db() {
-		$list = $this->default_icons;
-		$saved_list = $this->get_option( 'urls' );
+	function merge_icons_from_db() {
+		$output_list = $this->default_icons;
+		$saved_list = $this->get_option( 'icons' );
 		if ( $saved_list && is_array( $saved_list ) ) {
 			// I already have some settings saved
-			foreach ( $saved_list as $key => $value ) {
-				if ( array_key_exists( $key, $list ) )
-					$list[$key] = $value;
+			foreach ( $saved_list as $i => $icon ) {
+				// Check if this icon is still valid
+				foreach( $output_list as $j => $iconcheck ) {
+					if ( $iconcheck->name == $icon->name ) {
+						if ( ! empty( $icon->url ) ) $iconcheck->url = $icon->url;
+						if ( ! empty( $icon->text ) ) $iconcheck->text = $icon->text;
+						$output_list[$j] = $iconcheck;
+						break;
+					}
+				}
 			}
 		}
-		return $list;
+		return $output_list;
 	}
 
 
@@ -319,38 +370,50 @@ var meni_selected_icon;
 		// $result[0]: visible ones
 		// $result[1]: disabled ones
 		$result = array( array(), array() );
-		$urls = $this->merge_urls_from_db();
+		$icons = $this->merge_icons_from_db();
+		$added_list = array();
 
 		$en_order = $this->get_option( 'enabled_order' );
 		// Have I saved the icon order into the database?
 		if ( is_array( $en_order ) ) {
 			/* Visible icons ($result[0]) */
-			foreach ( $en_order as $code ) {
-				if ( array_key_exists( $code, $urls ) )
-					$result[0][$code] = $urls[$code];
+			foreach ( $en_order as $name ) {
+				for ( $i = 0; $i < sizeof( $icons ); $i++ ) {
+					$icon = $icons[$i];
+					if ( $icon->name == $name ) {
+						$result[0][] = $icon;
+						$added_list[] = $name;
+						break;
+					}
+				}
 			}
 
 			/* Disabled icons ($result[1]) */
 			$dis_order = $this->get_option( 'disabled_order' );
 			if ( is_array( $dis_order ) ) {
-				foreach ( $dis_order as $code ) {
-					if ( array_key_exists( $code, $urls ) )
-						$result[1][$code] = $urls[$code];
+				foreach ( $dis_order as $name ) {
+					for ( $i = 0; $i < sizeof( $icons ); $i++ ) {
+						$icon = $icons[$i];
+						if ( $icon->name == $name ) {
+							$result[1][] = $icon;
+							$added_list[] = $name;
+							break;
+						}
+					}
 				}
 			}
 			/* Icons that aren't in any array (errors?)
 			 * Added to disabled icons ($result[1]) */
-			foreach ( $urls as $code => $url ) {
-				if ( ! array_key_exists( $code, $result[0] ) &&
-				     ! array_key_exists( $code, $result[1] ) ) {
-					$result[1][$code] = $url;
+			foreach ( $icons as $i => $icon ) {
+				if ( ! in_array( $icon->name, $added_list ) ) {
+					$result[1][] = $icon;
 				}
 			}
 			return $result;
 		}
 		else {
 			// No order set. Return the default order.
-			$result[1] = $urls;
+			$result[1] = $icons;
 			return $result;
 		}
 	}
@@ -372,16 +435,21 @@ var meni_selected_icon;
 	/**
 	 * Receives the urls through Ajax, and saves them to the database.
 	 */
-	function save_ajax_urls() {
-		check_ajax_referer( 'meni_urls' );
+	function save_ajax_icons() {
+		check_ajax_referer( 'meni_icons' );
 		if ( ! current_user_can( 'manage_options' ) ) die( '1' );
 
-		$urls = $this->merge_urls_from_db();
-		foreach ( $_POST as $key => $value ) {
-			if ( array_key_exists( $key, $urls ) )
-				$urls[$key] = $value;
+		$icons = $this->merge_icons_from_db();
+		$name = $_POST['name'];
+		foreach ( $icons as $i => $icon ) {
+			if ( $icon->name == $name ) {
+				if ( ! empty( $_POST['url'] ) ) $icon->url = $_POST['url'];
+				if ( ! empty( $_POST['text'] ) ) $icon->text = $_POST['text'];
+				$icons[$i] = $icon;
+				break;
+			}
 		}
-		$this->update_option( 'urls', $urls );
+		$this->update_option( 'icons', $icons );
 		die( '0' );
 	}
 
@@ -421,6 +489,6 @@ add_filter( 'plugin_action_links', array( &$mystique_eni, 'add_action_link' ) );
 add_action( 'wp_ajax_meni_saveorder', array( &$mystique_eni, 'save_ajax_order' ) );
 
 // ... and when saving urls
-add_action( 'wp_ajax_meni_saveurls', array( &$mystique_eni, 'save_ajax_urls' ) );
+add_action( 'wp_ajax_meni_saveicons', array( &$mystique_eni, 'save_ajax_icons' ) );
 
 ?>
